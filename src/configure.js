49960c8679ecba9ea15211d4750ccf2d4564c8a0
Sheldon Welinga <sheldonwelinga@gmail.com>
2020-11-24 17:10:58 +0300

    updated the configuration and added security meta tag

--- src/configure.js
+++ src/configure.js
@@ -1,5 +1,5 @@
 // export const API_URL = "http://localhost:5000/api/v1";
 // export const IMG_URL = "http://localhost:5000";
 
-export const API_URL = "https://theparadisedreamer.herokuapp.com/api/v1";
-export const IMG_URL = "https://theparadisedreamer.herokuapp.com/";
+export const API_URL = "//theparadisedreamer.herokuapp.com/api/v1";
+export const IMG_URL = "//theparadisedreamer.herokuapp.com/";
