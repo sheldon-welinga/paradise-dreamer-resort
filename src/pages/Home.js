7440e27e6b2e9d45c1c6926178fd423131dc23e1
Sheldon Welinga <sheldonwelinga@gmail.com>
2021-06-11 10:08:03 +0300

    updated the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-import Gallery from "../components/Gallery";
+// import Gallery from "../components/Gallery";
 import HomeSlider from "../components/HomeSlider";
 import PopularDestinations from "../components/PopularDestinations";
 import SubscribeSection from "../components/SubscribeSection";
